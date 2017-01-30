563e40023373aa33faac169ff10021fe8d069239
THEERAWAT KAEWCHOTE <theerawatkc@gmail.com>
2017-01-31 01:40:50 +0700

    Add bootstrap in profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,15 +2,42 @@
 <html>
   <head>
     <meta charset="utf8">
+    <link rel="stylesheet" href="/css/app.css">
     <title>My Profile</title>
+    <style>
+      .img-profile {
+        max-width: 150px;
+        margin: auto;
+      }
+    </style>
   </head>
   <body>
-    <img src="images/profile.jpg">
-    <br>
-    Name : Theerawat Kaewchote (Por)
-    <br>
-    Full Stack Developer - TMES.Co.,Ltd
-    <br>
-    Medium : <a href="https://medium.com/@aimoocpe">@aimoocpe</a>
+    <div class="container">
+      <div class="jumbotron">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="images/profile.jpg" class="img-responsive img-circle img-profile">
+          </div>
+          <div class="col-md-8">
+            <h2>Theerawat Kaewchote (Por)</h2>
+            <li>Full Stack Developer - TMES Co.,Ltd.</li>
+            <li>Medium : <a href="https://medium.com/@aimoocpe">@aimoocpe</a></li>
+            <hr>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-md-4">
+            <img src="images/profile.jpg" class="img-responsive img-circle img-profile">
+          </div>
+          <div class="col-md-8">
+            <h2>Theerawat Kaewchote (Por)</h2>
+            <li>Full Stack Developer - TMES Co.,Ltd.</li>
+            <li>Medium : <a href="https://medium.com/@aimoocpe">@aimoocpe</a></li>
+            <hr>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
 </html>

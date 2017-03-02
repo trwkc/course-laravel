6d8d3a6574ea5ad4dbe57d02ab791d345f97c5b0
THEERAWAT KAEWCHOTE <theerawatkc@gmail.com>
2017-03-02 23:28:34 +0700

    Create reminder component

---
+++ resources/views/components/reminder.blade.php
@@ -0,0 +1,13 @@
+<form method="post" action="reminder/new">
+  <fieldset>
+    <legend>New Reminder:</legend>
+    <div class="form-group">
+      <label id="textReminder">Reminder</label>
+      <textarea type="text" id="textReminder" rows="8" class="form-control"></textarea>
+    </div>
+    <div class="form-group">
+      {{csrf_field()}}
+      <button type="button" class="btn btn-success">Create</button>
+    </div>
+  </fieldset>
+</form>

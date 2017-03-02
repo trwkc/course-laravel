efc5580df6f45fd8f0e6113768ed44ad40abe5a8
THEERAWAT KAEWCHOTE <theerawatkc@gmail.com>
2017-03-03 01:09:58 +0700

    Create task component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,24 +3,6 @@
 
   @include('components.reminder')
 
-  <h3>Task</h3>
-  <div class="panel panel-default">
-    <div class="panel-body">
-      @foreach($tasks as $task)
-        <div class="well"> 
-          <div class="media">
-            <div class="media-body">
-              <h4>{{$task}}</h4>
-            </div>
-            <div class="media-left media-middle">
-              <button type="button" class="btn btn-success pull-right">
-                <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-              </button>
-            </div>
-          </div>
-        </div>
-      @endforeach
-    </div>
-  </div>
-
+  @include('components.task')
+  
 @stop
